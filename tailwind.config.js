8357de2b75e5dee8032a6906536cde959460cd6c
AngelosZar <129210112+AngelosZar@users.noreply.github.com>
2024-11-15 20:28:55 +0100

    color pallete

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,22 @@ export default {
         primary: ['Inter', 'sans-serif'],
         secondary: ['Source Sans 3', 'sans-serif'],
       },
+      colors: {
+        body: {
+          dark: '#0B1423',
+          light: '#FDFDFD',
+        },
+        green: {
+          1: '#6FB15F',
+        },
+        'dark-purple': '#5333F6',
+
+        'dark-blue': '#07193C',
+        text: {
+          white: '#FDFDFD',
+          black: '#000000',
+        },
+      },
     },
   },
   plugins: [],
