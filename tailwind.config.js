ad81721d68568c1db54fe40456fd0f5414151631
AngelosZar <129210112+AngelosZar@users.noreply.github.com>
2024-11-15 16:11:44 +0100

    insert font primary and font secondary

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 export default {
   content: ['./index.html', './src/**/*.{js,css}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        primary: ['Inter', 'sans-serif'],
+        secondary: ['Source Sans 3', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 };
